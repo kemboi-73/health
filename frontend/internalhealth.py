ae1a201ba8ec58e1c8541a16cb74e3631d6046d0
Kemboi Mike <123319720+kemboi-73@users.noreply.github.com>
2024-07-11 16:58:09 +0300

    initial commit

---
+++ frontend/internalhealth.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# import streamlit as st
+
+import streamlit as st
+from backend import cohere_emotional_analysis as health
+
+emoji = {
+    'love' : ':hearts:',
+    'anger' : ':angry:',
+    'joy' : ':joy:',
+    'sadness' : ':sad:',
+    'surprise' : ':open_mouth:'
+}
+
+# Set page title
+st.set_page_config(page_title="Internal Health", page_icon=":bar_chart:")
+
+# Set page layout
+st.markdown("<h1 style='text-align: center;'>Internal Health</h1>", unsafe_allow_html=True)
+
+# Create user input section
+user_input = st.text_area("Enter your the user post:")
+
+if st.button("Generate reply"):
+    with st.spinner('Analysing post...'):
+        emotion = health.classify_emotion(user_input)
+
+        st.subheader('Suggested Reply')
+        st.write(health.write_post(user_input))
+        col1, col2 = st.columns(2)
+        with col1:
+            st.subheader('Emotional Level')
+            st.bar_chart(health.plotter(user_input))
+        with col2:
+            st.subheader('Emotion')
+            st.write(f'This person currently has a feeling {emotion} {emoji.get(emotion)}')
